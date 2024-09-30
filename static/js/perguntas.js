223f871a4cf58122088538c412aaa0bbf21fd7f5
bruno <buritibruno7@gmail.com>
2024-09-30 16:19:30 -0300

    30/09/2024
    esse e um jogo da forca sobre spam em gmail

---
+++ static/js/perguntas.js
@@ -0,0 +1,25 @@
+const palavras = [
+    "indesejadas",
+    "algoritmos",
+    "recuperar",
+    "marcar",
+    "sim",
+    "cuidado",
+    "trocar",
+    "alertas",
+    "desconhecidos",
+    "agrupamento",
+];
+
+const dicas = [
+    " o que é considerado spam no gmail",
+    "como o gmail filtra mensagens de spam",
+    "o que fazer se uma mensagem importante for para a pasta de spam",
+    "como posso denunciar um e-mail como spam no gmail",
+    "é possível recuperar mensagens que foram marcadas como spam por engano",
+    "como posso evitar receber spam no meu gmail",
+    "o que fazer se alguém estiver usando meu endereço de e-mail para enviar spam",
+    "como o gmail lida com phishing e tentativas de fraude",
+    "quais são os sinais de que um e-mail pode ser spam",
+    "o que é a aba promos e como ela se relaciona com o spam",
+];
